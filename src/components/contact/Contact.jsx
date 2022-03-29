703025f2768b672f48eacf669004c4f351edfc60
ysYassine <66660853+ysYassine@users.noreply.github.com>
2022-03-29 15:54:07 +0100

    contact content

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,57 @@
 import React from "react";
 import { CONTACT_REF } from "../../utils/sharedVariables";
 import "./contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { BsWhatsapp } from "react-icons/bs";
 
 const Contact = () => {
-  return <section id={CONTACT_REF}>contact</section>;
+  return (
+    <section id={CONTACT_REF}>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>dummy@test.test</h5>
+            <a href="mailto:dummy@test.test">Send a message</a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp className="contact__option-icon" />
+            <h4>WhatsApp</h4>
+            <h5>+123456789</h5>
+            <a
+              href="https://api.whatsapp.com/send?phone=+21611222333"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a message
+            </a>
+          </article>
+        </div>
+        <form action="">
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button className="btn btn-primary" type="submit">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
